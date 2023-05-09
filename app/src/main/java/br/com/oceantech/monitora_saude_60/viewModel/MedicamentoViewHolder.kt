a1e630812e8714e9fe69ee97cd9b10848c9d1820
gisele <gisesonia@gmail.com>
2023-05-09 18:07:12 -0300

    Visualizando o nome

--- app/src/main/java/br/com/oceantech/monitora_saude_60/viewModel/MedicamentoViewHolder.kt
+++ app/src/main/java/br/com/oceantech/monitora_saude_60/viewModel/MedicamentoViewHolder.kt
@@ -10,9 +10,7 @@ import br.com.oceantech.monitora_saude_60.model.Medicamento
 class MedicamentoViewHolder(private val binding: ItemMedicamentoBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(medicamento: Medicamento, onDelete: (Medicamento) -> Unit, onEdit: (Medicamento) -> Unit) {
 
-        binding.tvNomeMedicamento.text = itemView.context.getString(R.string.medication_name, medicamento.nome)
-
-        //binding.tvNomeMedicamento.text = "Nome do remédio: ${medicamento.nome}"
+        binding.tvNomeMedicamento.text = "Nome do remédio: ${medicamento.nome}"
         binding.tvDosagemMedicamento.text = "Dosagem: ${medicamento.dosagem}"
         binding.tvIntervaloMedicamento.text = "Intervalo entre doses: ${medicamento.intervaloDoses}"
         binding.tvHorariosMedicamento.text = "Horários: ${medicamento.horarios.joinToString(", ")}"
